8efaeb3a6cab79e307b17bcf302e1c8de9c0de99
Guilherme Cordeiro <guicsc1@hotmail.com>
2014-05-22 16:10:10 -0300

    Layout minigame 5.1

--- js/clock.js
+++ js/clock.js
@@ -1,145 +1,98 @@
-(function($){
-    // A global object used by the functions of the plug-in:
-    var gVars = {};
+var gVars = {}
 
-    // Extending the jQuery core:
+function setUp(color_id, selector, value)
+{
+  // The colors of the dials:
+  var colors = ['orange','blue','green', "red", "yellow"];
+  var tmp;
+  tmp = $('<div>').attr('class',colors[color_id]+' clock').html(
+      '<div class="display"></div>'+
+      '<div class="front left"></div>'+
+      '<div class="rotate left">'+
+      '<div class="bg left"></div>'+
+      '</div>'+
+      '<div class="rotate right">'+
+      '<div class="bg right"></div>'+
+      '</div>'
+  );
+  $(selector).append(tmp);
+  // Assigning some of the elements as variables for speed:
+  tmp.rotateLeft = tmp.find('.rotate.left');
+  tmp.rotateRight = tmp.find('.rotate.right');
+  tmp.display = tmp.find('.display');
+  gVars[colors[color_id]] = tmp;
 
-    $.fn.tzineClock = function(opts){
+  var currentTime = new Date();
+  var h = currentTime.getHours();
+  var m = currentTime.getMinutes();
+  var s = currentTime.getSeconds();
 
-        // "this" contains the elements that were selected when calling the plugin: 		$('elements').tzineClock();
-        // If the selector returned more than one element, we use the first one:
-        var container = this.eq(0);
-        if(!container)
-        {
-            try{
-                console.log("Invalid selector!");
-            } catch(e){}
+  animation(gVars[colors[color_id]], value , 12);
+}
 
-            return false;
-        }
+function updateView(clocks){
+  var colors = ['orange','blue','green', "red", "yellow"];
+  animation(gVars[colors[0]], clocks[0] , 12);
+  animation(gVars[colors[1]], clocks[1] , 12);
+  animation(gVars[colors[2]], clocks[2] , 12);
+  animation(gVars[colors[3]], clocks[3] , 12);
+  animation(gVars[colors[4]], clocks[4] , 12);
 
-        if(!opts) opts = {};
+}
 
-        var defaults = {
-            /* Additional options will be added in future versions of the plugin. */
-        };
+function animation(clock, current, total){
+  // Calculating the current angle:
+  var angle = (360/total)*(current);
+  var element;
+  if(current==0)
+  {
+      // Hiding the right half of the background:
+      clock.rotateRight.hide();
 
-        /* Merging the provided options with the default ones (will be used in future versions of the plugin): */
+      // Resetting the rotation of the left part:
+      rotateElement(clock.rotateLeft,0);
+  }
 
-        $.each(defaults,function(k,v){
-            opts[k] = opts[k] || defaults[k];
-        });
+  if(angle<=180)
+  {
+      // The left part is rotated, and the right is currently hidden:
+      element = clock.rotateLeft;
+  }
+  else
+  {
+      // The first part of the rotation has completed, so we start rotating the right part:
+      clock.rotateRight.show();
+      clock.rotateLeft.show();
 
-        // Calling the setUp function and passing the container,
-        // will be available to the setUp function as "this":
+      rotateElement(clock.rotateLeft,180);
+      element = clock.rotateRight;
 
-        setUp.call(container);
+      angle = angle-180;
+  }
 
-        return this;
-    }
+  rotateElement(element,angle);
+  // Setting the text inside of the display element, inserting a leading zero if needed:
+  clock.display.html(current<10?'0'+current:current);
+}
 
-    function setUp()
-    {
-        // The colors of the dials:
-        var colors = ['orange','blue','green'];
+function rotateElement(element,angle)
+{
+  // Rotating the element, depending on the browser:
+  var rotate = 'rotate('+angle+'deg)';
+  if(element.css('MozTransform')!=undefined)
+      element.css('MozTransform',rotate);
 
-        var tmp;
-        for(var i=0;i<3;i++)
-        {
-            // Creating a new element and setting the color as a class name:
+  else if(element.css('WebkitTransform')!=undefined)
+      element.css('WebkitTransform',rotate);
 
-            tmp = $('<div>').attr('class',colors[i]+' clock').html(
-                '<div class="display"></div>'+
-                '<div class="front left"></div>'+
-                '<div class="rotate left">'+
-                '<div class="bg left"></div>'+
-                '</div>'+
-                '<div class="rotate right">'+
-                '<div class="bg right"></div>'+
-                '</div>'
-            );
+  // A version for internet explorer using filters, works but is a bit buggy (no surprise here):
 
-            // Appending to the fancyClock container:
-            $(this).append(tmp);
-
-            // Assigning some of the elements as variables for speed:
-            tmp.rotateLeft = tmp.find('.rotate.left');
-            tmp.rotateRight = tmp.find('.rotate.right');
-            tmp.display = tmp.find('.display');
-
-            // Adding the dial as a global variable. Will be available as gVars.colorName
-            gVars[colors[i]] = tmp;
-        }
-
-        // Setting up a interval, executed every 1000 milliseconds:
-        setInterval(function(){
-
-            var currentTime = new Date();
-            var h = currentTime.getHours();
-            var m = currentTime.getMinutes();
-            var s = currentTime.getSeconds();
-
-            animation(gVars.green, s, 60);
-            animation(gVars.blue, m, 60);
-            animation(gVars.orange, h, 24);
-        },1000);
-    }
-
-    function animation(clock, current, total){
-        // Calculating the current angle:
-        var angle = (360/total)*(current+1);
-        var element;
-        if(current==0)
-        {
-            // Hiding the right half of the background:
-            clock.rotateRight.hide();
-
-            // Resetting the rotation of the left part:
-            rotateElement(clock.rotateLeft,0);
-        }
-
-        if(angle<=180)
-        {
-            // The left part is rotated, and the right is currently hidden:
-            element = clock.rotateLeft;
-        }
-        else
-        {
-            // The first part of the rotation has completed, so we start rotating the right part:
-            clock.rotateRight.show();
-            clock.rotateLeft.show();
-
-            rotateElement(clock.rotateLeft,180);
-            element = clock.rotateRight;
-
-            angle = angle-180;
-        }
-
-        rotateElement(element,angle);
-
-        // Setting the text inside of the display element, inserting a leading zero if needed:
-        clock.display.html(current<10?'0'+current:current);
-    }
-
-    function rotateElement(element,angle)
-    {
-        // Rotating the element, depending on the browser:
-        var rotate = 'rotate('+angle+'deg)';
-        if(element.css('MozTransform')!=undefined)
-            element.css('MozTransform',rotate);
-
-        else if(element.css('WebkitTransform')!=undefined)
-            element.css('WebkitTransform',rotate);
-
-        // A version for internet explorer using filters, works but is a bit buggy (no surprise here):
-
-        else if(element.css("filter")!=undefined)
-        {
-            var cos = Math.cos(Math.PI * 2 / 360 * angle);
-            var sin = Math.sin(Math.PI * 2 / 360 * angle);
-            element.css("filter","progid:DXImageTransform.Microsoft.Matrix(M11="+cos+",M12=-"+sin+",M21="+sin+",M22="+cos+",SizingMethod='auto expand',FilterType='nearest neighbor')");
-            element.css("left",-Math.floor((element.width()-200)/2));
-            element.css("top",-Math.floor((element.height()-200)/2));
-        }
-    }
-})(jQuery)
+  else if(element.css("filter")!=undefined)
+  {
+      var cos = Math.cos(Math.PI * 2 / 360 * angle);
+      var sin = Math.sin(Math.PI * 2 / 360 * angle);
+      element.css("filter","progid:DXImageTransform.Microsoft.Matrix(M11="+cos+",M12=-"+sin+",M21="+sin+",M22="+cos+",SizingMethod='auto expand',FilterType='nearest neighbor')");
+      element.css("left",-Math.floor((element.width()-200)/2));
+      element.css("top",-Math.floor((element.height()-200)/2));
+  }
+}
--- js/levels/level5.js
+++ js/levels/level5.js
@@ -6,42 +6,46 @@
    [1, 1, -1, 3, 1],
    [0, 1, 0, 2, 3]]
 
+  function evaluateAlignment(){
+    if( clocks == [0,0,0,0,0])
+      return true;
+    return false;
+  }
+
 function updateValues(direction, clock_id){
+   pre_clocks = [0,0,0,0,0]
    if(direction == 1){
-      clocks[0] = (clocks[0] + control[clock_id][0])%12;
-      clocks[1] = (clocks[1] + control[clock_id][1])%12;
-      clocks[2] = (clocks[2] + control[clock_id][2])%12;
-      clocks[3] = (clocks[3] + control[clock_id][3])%12;
-      clocks[4] = (clocks[4] + control[clock_id][4])%12;
+     pre_clocks[0] = (clocks[0] + control[clock_id][0])%12;
+     pre_clocks[1] = (clocks[1] + control[clock_id][1])%12;
+     pre_clocks[2] = (clocks[2] + control[clock_id][2])%12;
+     pre_clocks[3] = (clocks[3] + control[clock_id][3])%12;
+     pre_clocks[4] = (clocks[4] + control[clock_id][4])%12;
     }else{
-      clocks[0] = (clocks[0] - control[clock_id][0])%12;
-      clocks[1] = (clocks[1] - control[clock_id][1])%12;
-      clocks[2] = (clocks[2] - control[clock_id][2])%12;
-      clocks[3] = (clocks[3] - control[clock_id][3])%12;
-      clocks[4] = (clocks[4] - control[clock_id][4])%12;
+     pre_clocks[0] = (clocks[0] - control[clock_id][0])%12;
+     pre_clocks[1] = (clocks[1] - control[clock_id][1])%12;
+     pre_clocks[2] = (clocks[2] - control[clock_id][2])%12;
+     pre_clocks[3] = (clocks[3] - control[clock_id][3])%12;
+     pre_clocks[4] = (clocks[4] - control[clock_id][4])%12;
     }
-
-
+  clocks[0] = (pre_clocks[0] < 0) ? 12 + pre_clocks[0] : pre_clocks[0];
+  clocks[1] = (pre_clocks[1] < 0) ? 12 + pre_clocks[1] : pre_clocks[1];
+  clocks[2] = (pre_clocks[2] < 0) ? 12 + pre_clocks[2] : pre_clocks[2];
+  clocks[3] = (pre_clocks[3] < 0) ? 12 + pre_clocks[3] : pre_clocks[3];
+  clocks[4] = (pre_clocks[4] < 0) ? 12 + pre_clocks[4] : pre_clocks[4];
 }
 
 function controlClick(direction, clock_id){
   updateValues(direction, clock_id);
-  updateView();
+  updateView(clocks);
 }
 
-function updateView(){
-   $("#clock_one").html(clocks[0] + "");
-   $("#clock_two").html(clocks[1] + "");
-   $("#clock_three").html(clocks[2] + "");
-   $("#clock_four").html(clocks[3] + "");
-   $("#clock_five").html(clocks[4] + "");
-}
+
 
 
 function scramble(){
   for(i = 0; i < 10; i++){
     random_id = Math.floor(Math.random()*5);
-    console.log(random_id + "");
+
     random_direction = Math.floor(Math.random()*10 + 1)%2;
     updateValues(random_direction, random_id);
 
@@ -74,7 +78,7 @@ game.Level5 = {
       HideMelonJS();
       console.log("Setting level 5 map");
       $("#game").append("<div id='level5'></div>");
-      $("#game").append("<div id='level5_controls'></div>");
+      $("#game").append("<div id='level5_controls' class='controls' ></div><div class='clearBoth></div>");
       this.play();
     },
 
@@ -89,24 +93,35 @@ game.Level5 = {
       $("#level5").append("<div id='clock_three'></div>");
       $("#level5").append("<div id='clock_four'></div>");
       $("#level5").append("<div id='clock_five'></div>");
-      $("#level5_controls").append("<p>Primeiro Controle</p><input type='button' id='control1l' value='Left' onClick='controlClick(0,0)'></input><input type='button' id='control1r' value='Right' onClick='controlClick(1,0)'></input>" +
-      "<p>Segundo Controle</p><input type='button' id='control2l' value='Left' onClick='controlClick(0,1)'></input><input type='button' id='control2r' value='Right' onClick='controlClick(1,1)'></input><br>" +
-      "<p>Terceiro Controle</p><input type='button' id='control3l' value='Left' onClick='controlClick(0,2)'></input><input type='button' id='control3r' value='Right' onClick='controlClick(1,2)'></input><br>" +
-      "<p>Quarto Controle</p><input type='button' id='control4l' value='Left' onClick='controlClick(0,3)'></input><input type='button' id='control4r' value='Right' onClick='controlClick(1,3)'></input><br>" +
-      "<p>Quinto Controle</p><input type='button' id='control5l' value='Left' onClick='controlClick(0,4)'></input><input type='button' id='control5r' value='Right' onClick='controlClick(1,4)'></input><br>");
-      $("#clock_one").append(clocks[0] + "");
-      $("#clock_two").append(clocks[1] + "");
-      $("#clock_three").append(clocks[2] + "");
-      $("#clock_four").append(clocks[3] + "");
-      $("#clock_five").append(clocks[4] + "");
-    //  $("#clock_one").tzineClock();
-    //  $("#clock_two").tzineClock();
-    //  $("#clock_three").tzineClock();
-    //  $("#clock_four").tzineClock();
-    //  $("#clock_five").tzineClock();
+      $("#level5_controls").append("<div id='control_one' class='controls controls_size'></div>"
+          + "<div id='control_two' class='controls controls_size'></div>"
+          + "<div id='control_three' class='controls controls_size'></div>"
+          + "<div id='control_four' class='controls controls_size'></div>"
+          + "<div id='control_five' class='controls controls_size'></div>")
+
+     $("#control_one").append("<p>Primeiro Controle</p><input type='button' id='control1l' value='Left' onClick='controlClick(0,0)'></input><input type='button' id='control1r' value='Right' onClick='controlClick(1,0)'></input>");
+     $("#control_two").append("<p>Segundo Controle</p><input type='button' id='control2l' value='Left' onClick='controlClick(0,1)'></input><input type='button' id='control2r' value='Right' onClick='controlClick(1,1)'></input><br>");
+     $("#control_three").append("<p>Terceiro Controle</p><input type='button' id='control3l' value='Left' onClick='controlClick(0,2)'></input><input type='button' id='control3r' value='Right' onClick='controlClick(1,2)'></input><br>");
+      $("#control_four").append("<p>Quarto Controle</p><input type='button' id='control4l' value='Left' onClick='controlClick(0,3)'></input><input type='button' id='control4r' value='Right' onClick='controlClick(1,3)'></input><br>");
+      $("#control_five").append("<p>Quinto Controle</p><input type='button' id='control5l' value='Left' onClick='controlClick(0,4)'></input><input type='button' id='control5r' value='Right' onClick='controlClick(1,4)'></input><br>");
+      $("#level5_control").click(function(){
+        if(evaluateAlignment()){
+          me.state.change(me.state.MENU);
+        }
+      })
+
+      setUp(0, "#clock_one", clocks[0]);
+      setUp(1, "#clock_two", clocks[1]);
+      setUp(2, "#clock_three", clocks[2]);
+      setUp(3, "#clock_four", clocks[3]);
+      setUp(4, "#clock_five", clocks[4]);
     },
 
+
+
   }),
+
+
 
 };
 
